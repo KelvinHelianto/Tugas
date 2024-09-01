1942ef34ac64ef8b5ce037bc49cd4214de4cf8c8
unknown <khelianto@gmail.com>
2024-09-01 20:35:40 +0900

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>Valet</title>
+
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,600,700&display=swap" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+  <div class="hero_area">
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container">
+          <a class="navbar-brand" href="index.html">
+            <span>
+              Valet
+            </span>
+          </a>
+
+          <div class="navbar-collapse" id="">
+            <div class="user_option">
+              <a href="">
+                Login
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
+    <!-- slider section -->
+    <section class=" slider_section position-relative">
+      <div class="slider_container">
+        <div class="img-box">
+          <img src="images/hero-img.jpg" alt="">
+        </div>
+        <div class="detail_container">
+          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <div class="detail-box">
+                  <h1>
+                    Valet <br>
+                    Your <br>
+                    Car
+                  </h1>
+                  <a href="">
+                    Contact Us
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- end slider section -->
+  </div>
+  <!-- book section -->
+  <section class="book_section">
+    <div class="form_container">
+      <form action="">
+        <div class="form-row">
+          <div class="col-lg-8">
+            <div class="form-row">
+              <div class="col-md-6">
+                <label for="parkingName">Pick Up Location</label>
+                <input type="text" class="form-control" placeholder="acb ">
+              </div>
+              <div class="col-md-6">
+                <label for="parkingNumber">Drop Location</label>
+                <input type="text" class="form-control" placeholder="acb ">
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="col-md-6">
+                <label for="parkingName">Pick Up Date</label>
+                <input type="text" class="form-control" placeholder="07/09/2020">
+              </div>
+              <div class="col-md-6">
+                <label for="parkingNumber">Return Date</label>
+                <input type="text" class="form-control" placeholder="07/09/2020">
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="btn-container">
+              <button type="submit" class="">
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+
+      </form>
+    </div>
+    <div class="img-box">
+      <img src="images/book-car.png" alt="">
+    </div>
+  </section>
+
+  <!-- end book section -->
+
+  <!-- about section -->
+
+  <section class="about_section layout_padding-bottom">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-7 px-0">
+          <div class="img-box">
+            <img src="images/about-img.png" alt="">
+          </div>
+        </div>
+        <div class="col-md-4 col-lg-3">
+          <div class="detail-box">
+            <h2>
+              About Us
+            </h2>
+            <p>
+            Welcome to Valet, where convenience meets exceptional service. We understand that your time is valuable, and our mission is to provide seamless valet parking solutions that elevate your experience, whether at events, hotels, restaurants, or any venue.
+            </p>
+            <a href="">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end about section -->
+
+
+  <!-- best section -->
+
+  <section class="best_section">
+    <div class="container">
+      <div class="book_now">
+        <div class="detail-box">
+          <h2>
+            Use Valet Now!
+          </h2>
+        </div>
+        <div class="btn-box">
+          <a href="">
+            Book 
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end best section -->
+
+  <!-- us section -->
+
+  <section class="us_section">
+    <div class="container">
+      <div class="heading_container">
+        <h2>
+          Why choose Us
+        </h2>
+      </div>
+    </div>
+    <div class="us_container layout_padding2">
+      <div class="content_box">
+        <div class="box">
+          <div class="img-box">
+            <img src="images/u-1.png" alt="">
+          </div>
+          <div class="detail-box">
+            <h5>
+              Low Rent
+            </h5>
+          </div>
+        </div>
+        <div class="box">
+          <div class="img-box">
+            <img src="images/u-2.png" alt="">
+          </div>
+          <div class="detail-box">
+            <h5>
+              Fast Car
+            </h5>
+          </div>
+        </div>
+        <div class="box">
+          <div class="img-box">
+            <img src="images/u-3.png" alt="">
+          </div>
+          <div class="detail-box">
+            <h5>
+              Safe Car
+            </h5>
+          </div>
+        </div>
+        <div class="box">
+          <div class="img-box">
+            <img src="images/u-4.png" alt="">
+          </div>
+          <div class="detail-box">
+            <h5>
+              Quick Support
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end us section -->
+
+  <!-- contact section -->
+
+  <section class="contact_section layout_padding">
+    <div class="container">
+      <div class="heading_container">
+        <h2>
+          Contact Us
+        </h2>
+      </div>
+      <div class="row">
+        <div class="col-md-8 mx-auto">
+          <div class="form_container">
+            <form>
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                  <input type="text" class="form-control" id="inputName4" placeholder="Nama ">
+                </div>
+                <div class="form-group col-md-6">
+                  <input type="text" class="form-control" id="inputSubject4" placeholder="Nomor Telepon">
+                </div>
+              </div>
+              <div class="form-row">
+                <div class="form-group col">
+                  <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                </div>
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" id="inputMessage" placeholder="Pesan">
+              </div>
+              <div class="d-flex justify-content-center">
+                <button type="submit" class="">Send</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="contact_items">
+
+        <a href="">
+          <div class="img-box">
+            <img src="images/location.png" alt="">
+          </div>
+          <h6>
+            PT AKSII
+          </h6>
+        </a>
+        <a href="">
+          <div class="img-box">
+            <img src="images/call.png" alt="">
+          </div>
+          <h6>
+            0888888888888
+          </h6>
+        </a>
+        <a href="">
+          <div class="img-box">
+            <img src="images/mail.png" alt="">
+          </div>
+          <h6>
+            kelvinsigma@gmail.com
+          </h6>
+        </a>
+
+      </div>
+    </div>
+  </section>
+
+  <!-- end contact section -->
+
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <script src="js/bootstrap.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+  </script>
+  <script src="js/custom.js"></script>
+
+
+</body>
+
+</html>
